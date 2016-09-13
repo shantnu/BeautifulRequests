3f6d3d99c4f3d47f61931af201ec13ef345def95
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-09-13 20:37:34 +0100

    1st ver

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='BeautifulRequests',
+      packages = ['BeautifulRequests'],
+      version='0.4',
+      description='Combine Beautiful Soup 4 and Requests for one super library',
+      url='https://github.com/shantnu/BeautifulRequests',
+      # download_url = 'https://github.com/shantnu/shantnu_joke/archive/0.4.zip',
+      author='Shantnu',
+      license='GPL',
+      )
